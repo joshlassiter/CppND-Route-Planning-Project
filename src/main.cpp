f5e06d188505ec3bf4fa2b17ea51c2a8e9c67028
joshlassiter <38287133+joshlassiter@users.noreply.github.com>
2022-09-26 18:40:58 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "route_model.h"
 #include "render.h"
 #include "route_planner.h"
+using namespace std;
 
 using namespace std::experimental;
 
@@ -51,16 +52,26 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
+    //Complete this TODO to satisfy Project Rubric Criterias of User Input
+  
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    cout << "Please enter start_x value: ";
+    cin >> start_x;
+    cout << "Please enter start_y value: ";
+    cin >> start_y;
+    cout << "Please enter end_x value: ";
+    cin >> end_x;
+    cout << "Please enter end_y value: ";
+    cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y };
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
